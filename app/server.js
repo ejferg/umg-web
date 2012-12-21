792b27e45a7a9e28fa9fb1eb4ac7678992668079
ejferguson <ejferg@gmail.com>
2012-12-20 21:01:42 -0500

    Website in node

--- app/server.js
+++ app/server.js
@@ -1,5 +1,36 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Umg Web');
-})
+var express = require('express');
+var app = express();
+
+app.configure(function(){
+    app.set('port', process.env.PORT || 3000);
+    app.set('views', __dirname + '/views');
+   // app.set('view engine', 'kiwi');
+    app.set('view engine', 'ejs');
+
+    app.use(express.favicon());
+    app.use(express.logger('dev'));
+    app.use(express.bodyParser());
+    app.use(express.methodOverride());
+
+    app.use(app.router);
+    app.use(express.static(__dirname + '/public'));
+});
+
+app.configure('development', function () {
+    app.use(express.errorHandler({dumpExceptions: true, showStack: true }));
+});
+
+app.configure('production', function () {
+    app.use(express.errorHandler());
+});
+
+app.all('/', function(req, res){
+
+    var fb = req.query['fb'];
+    console.log('fb: '+fb);
+    res.render('index', { fb: fb });
+});
+
+app.listen(app.get('port'), function onListen(){
+    console.log("Express server listening on port " + app.get('port'));
+});
